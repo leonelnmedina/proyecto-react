fe12b4a419e9876e9072eb50ad78e524698ada13
Leonel Medina <leonelnmedina@gmail.com>
2023-07-12 21:53:35 -0300

    clase 6- crads

--- src/components/pages/itemList/ItemListContainer.jsx
+++ src/components/pages/itemList/ItemListContainer.jsx
@@ -1,9 +1,13 @@
 import ItemList from "./ItemList";
 
 const tarea = new Promise((resolve, reject) => {
-  resolve();
-  reject();
+  resolve("Salio todo bien");
+  // reject("Salio todo mal");
 });
+
+tarea
+  .then((respuesta) => console.log(respuesta))
+  .catch((error) => console.log(error));
 
 const ItemListContainer = () => {
   return <ItemList />;
